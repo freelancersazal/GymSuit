f3f9a71d8d417d9e531676269a2b08c4d903a6cb
freelancersazal <freelancersazal@gmail.com>
2022-04-24 18:12:19 +0600

    Home page done and added home page-2 and about pages

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(function () {
     $('.test_slider').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 4000,
         dots: true,
         arrows: false,
@@ -62,12 +62,66 @@ $(function () {
     }); 
 
     
+    //*=======BANNER SLIDER====== 
+    $('.banner_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+    }); 
+
     
-    
-    
-    
-    
-    
+    //*=======SERVICE SLIDER====== 
+    $('.service_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }   
+        ]
+    }); 
+
+
+    //*=======BANNER SLIDER====== 
+    $('.info_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+    }); 
+
+
     
 
 });
